10b30f19d9a85d1f9429b5fa9575a339656bdeb8
SAPIENT\asi325 <asinha115@sapient.com>
2017-03-31 12:21:07 +0530

    Modification

--- Transformation.js
+++ Transformation.js
@@ -1,34 +1,34 @@
 var transformModule = (function(){
+    var tranApp = {}
+    var context,canvas,canvasHeight,canvasWidth;
 
-	var tranApp  ={};
+    tranApp.init = function(){
+        canvas = document.getElementById('mycanvas');
+        context = canvas.getContext('2d');
+        canvasHeight = canvas.height;
+        canvasWidth = canvas.width;
+    };
 
-	tranApp.init = function(){
-		var canvas = document.getElementById('myCanvas');
-		context = canvas.getContext('2d');
+    tranApp.translate = function(){
+        var lineGrad = context.createLinearGradient(0,0,150,0);
+        lineGrad.addColorStop(0,'red');
+        lineGrad.addColorStop(0.25,'green');
+        lineGrad.addColorStop(0.5,'blue');
+        lineGrad.addColorStop(1,'yellow');
+        context.fillStyle = lineGrad;
+        context.fillRect(20,20,100,50);
+        context.translate(50,0);
+        context.fillRect(20,20,100,50);
+        context.scale(1.5,1.5);
+        context.fillRect(20,20,100,50); 
+    };
 
-		canvasHeight = canvas.height;
-		canvasWidth = canvas.canvasWidth;
-	};
+    tranApp.rotate = function(){};
+    tranApp.scale = function(){};
+    tranApp.transform = function(){};
 
-	tranApp.translate = function(){
-		var lineGrad = context.createLinearGradient(0,0,150,0);
-
-		lineGrad.addColorStop(0,'red');
-		lineGrad.addColorStop(0.25,'green');
-		lineGrad.addColorStop(0.5,'blue');
-
-		context.fillStyle=lineGrad;
-
-		context.fillRect(20,20,100,50);
-	};
-
-	tranApp.rotate=function(){};
-	tranApp.scale=function(){};
-	tranApp.transform = function(){};
-
-	return tranApp;
-
-})()
+    return tranApp;
+})();
 
 transformModule.init();
-transformModule.translate(); 
+transformModule.translate();
